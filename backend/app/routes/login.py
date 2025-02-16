2aab5d40b93e9237456f5a11904cea6614db9d01
Ahmed Hatem <ahmadatimo2002@gmail.com>
2025-02-16 13:36:49 +0300

    updated test users

--- backend/app/routes/login.py
+++ backend/app/routes/login.py
@@ -10,8 +10,16 @@ class LoginRequest(BaseModel):
 
 # Simulated user database (replace with real database logic)
 fake_users_db = {
-    "test@example.com": {"password": "123456"}
+    "test@example.com": {"password": "123456"},
+    "mona@gmail.com": {"password": "mona"},
+    "john.doe@example.com": {"password": "password123"},
+    "alice.smith@example.com": {"password": "alice123"},
+    "bob.jones@example.com": {"password": "bobpass"},
+    "charlie.brown@example.com": {"password": "charlie2025"},
+    "eve.white@example.com": {"password": "eve1234"},
+    "david.green@example.com": {"password": "david2025"}
 }
+
 
 @router.post("/login")
 async def login(request: LoginRequest):
